5071c57c30532a22100b8547e79a9c05a5d2bedb
Aurelien CHIREN <aurelien.chiren@gmail.com>
2022-05-02 04:24:40 +0200

    handle add and delete goals

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const sampleGoals = [
@@ -15,13 +22,63 @@ export default function App() {
     "Faire un triathlon",
   ];
 
+  const [goal, setGoal] = useState("Je veux être plus riche qu'Elon");
+  const [goalsList, setGoalsList] = useState([]);
+
+  const handleChangeText = (e) => {
+    setGoal(e);
+  };
+
+  const handleOnPress = () => {
+    setGoalsList([...goalsList, goal]);
+    setGoal("");
+  };
+
+  const deleteItem = (index) => {
+    const newGoalList = goalsList.splice(index + 1, 1);
+    setGoalsList(newGoalList);
+  };
+
+  useEffect(() => {
+    console.log("goalList :", goalsList);
+  }, [goalsList]);
+
   return (
     <View style={styles.container}>
-      {sampleGoals.map((goal, index) => (
-        <Text key={index}>{goal}</Text>
-      ))}
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.formFields}>
+        <TextInput
+          style={styles.input}
+          value={goal}
+          clearTextOnFocus={true}
+          onChangeText={(e) => handleChangeText(e)}
+          placeholder={"Je veux être plus riche qu'Elon"}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={(e) => handleOnPress(e)}
+        >
+          <Text>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.goalListStyle}>
+        {goalsList &&
+          goalsList.map((goal, index) => (
+            <View style={styles.goalItem}>
+              <View style={styles.goalItemBackground}>
+                <Text key={index} style={styles.goalItemText}>
+                  {"\u2022"}
+                  {goal}
+                </Text>
+              </View>
+              <Text
+                style={styles.deleteCross}
+                onPress={() => deleteItem(index)}
+              >
+                x
+              </Text>
+            </View>
+          ))}
+      </View>
     </View>
   );
 }
@@ -32,5 +89,52 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    flexDirection: "column",
+  },
+  input: {
+    height: 50,
+    width: 250,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#DDDDDD",
+    width: 100,
+    height: 50,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  formFields: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  goalListStyle: {
+    marginTop: 25,
+    flexDirection: "column",
+  },
+  goalItemText: {
+    fontSize: 20,
+    padding: 5,
+    marginTop: 10,
+
+    color: "#FFFfFF",
+  },
+  goalItemBackground: {
+    borderRadius: 5,
+    backgroundColor: "#5B0C83",
+    marginVertical: 5,
+  },
+  deleteCross: {
+    fontSize: 35,
+    marginHorizontal: 5,
+    alignSelf: "center",
+    color: "#C71FE5",
+    borderRadius: 5,
+    //backgroundColor: "#5B0C83",
+  },
+  goalItem: {
+    flexDirection: "row",
   },
 });
